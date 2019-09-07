fc4dae0e36770c92457e6c89ad8736226debefd0
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-09-07 16:26:13 +0100

    Remove accidental duplication of DISPLAY envvar

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ sleep 1
 
 x11vnc -display :0 -usepw -forever &
 
-export DISPLAY=:0
 /opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD" &
 
 socat TCP-LISTEN:4002,fork TCP:localhost:4001,forever
