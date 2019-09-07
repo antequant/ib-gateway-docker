371ee6bf513b7b5fc3f34c47b7e2ec1017dfeccb
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-09-07 13:08:33 +0100

    Get everything onto the same X display

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 set -e
 set -o errexit
 
-x11vnc -usepw -create -forever &
+Xvfb :0 &
+x11vnc -display :0 -usepw -forever &
+
+export DISPLAY=:0
 /opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway --mode=paper
