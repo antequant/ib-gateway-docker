29207c3cb4c6523274338970562e5a7d3bef599a
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-08 11:38:20 +0000

    Re-order run.sh so gateway termination ends container

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ Xvfb :0 &
 sleep 1
 
 x11vnc -display :0 -usepw -forever &
+socat TCP-LISTEN:4002,fork TCP:localhost:4001,forever &
 
-/opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD" &
-
-socat TCP-LISTEN:4002,fork TCP:localhost:4001,forever
+# Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
+# Retry behavior can be implemented by re-running the container.
+/opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD"
