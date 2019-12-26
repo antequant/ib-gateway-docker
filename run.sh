4eb5316697653bd8f76e431de1c5b2d6f8420e4a
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-26 12:42:25 +0000

    Allow TWS and VNC ports to be configured

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ set -o errexit
 Xvfb :0 &
 sleep 1
 
-x11vnc -display :0 -usepw -forever &
-socat TCP-LISTEN:4002,fork TCP:localhost:4001,forever &
+x11vnc -rfbport $VNC_PORT -display :0 -usepw -forever &
+socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
 
 # Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
 # Retry behavior can be implemented by re-running the container.
